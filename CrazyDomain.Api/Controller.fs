ccd8dd90de4f6f3586f8b050944b6c90519c430c
Jeremiah Redekop <jeremiah@geniuscode.net>
2014-04-13 20:47:26 -0700

    2.15 - Envelope, Envelope Wrapper, and Publish-Subscribe

--- CrazyDomain.Api/Controller.fs
+++ CrazyDomain.Api/Controller.fs
@@ -1,8 +1,10 @@
 ﻿namespace CrazyDomain.Api
 
+open System
 open System.Net
 open System.Net.Http
 open System.Web.Http
+open System.Reactive.Subjects
 
 type HomeController() = 
     inherit ApiController()
@@ -10,6 +12,20 @@ type HomeController() =
 
 type ReservationController() = 
     inherit ApiController()
+    let subject = new Subject<Envelope<MakeReservation>>()
     member this.Post (rendition : MakeReservationRendition) = 
+        let cmd = 
+            {
+                MakeReservation.Date = DateTime.Parse rendition.Date
+                Name = rendition.Date
+                Email = rendition.Email
+                Quantity = rendition.Quantity
+            }
+            |> EnvelopeWithDefaults
+        subject.OnNext cmd
         new HttpResponseMessage(HttpStatusCode.Accepted)
-
+    interface IObservable<Envelope<MakeReservation>> with
+        member this.Subscribe observer = subject.Subscribe observer
+    override this.Dispose disposing =
+        if disposing then subject.Dispose()
+        base.Dispose disposing
---
+++ CrazyDomain.Api/Messages.fs
@@ -0,0 +1,29 @@
+﻿namespace CrazyDomain.Api
+
+open System
+
+[<CLIMutable>]
+type MakeReservation = {
+    Date : DateTime
+    Name : string
+    Email : string
+    Quantity : int }
+
+[<AutoOpenAttribute>]
+module Envelope =
+    [<CLIMutable>]
+    type Envelope<'T> ={
+        Id : Guid
+        Created : DateTimeOffset
+        Item : 'T
+    }
+
+    let Envelope id created item = {
+        Id = id
+        Created = created
+        Item = item
+    }
+
+    let EnvelopeWithDefaults item = 
+        Envelope (Guid.NewGuid()) (DateTimeOffset.Now) item
+    
